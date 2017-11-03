45e54fa191d14fb164daa9dde2e02b6f9160ab37
이윤걸(YunGeol Lee)/개발혁신팀/SKP <lygirl@sk.com>
2017-11-03 14:42:07 +0900

    initail commit

---
+++ src/main/java/com/example/ContractsOnTheProducerSide/ContractsOnTheProducerSideApplication.java
@@ -0,0 +1,12 @@
+package com.example.ContractsOnTheProducerSide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractsOnTheProducerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractsOnTheProducerSideApplication.class, args);
+	}
+}
